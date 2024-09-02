37f0261fe3fd017044a6bcccf315687e4fea04ed
eljaguar1 <96393403+eljaguar1@users.noreply.github.com>
2024-09-02 15:15:59 -0400

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,3 +1,3 @@
-api_id = 10181262
-api_hash = "f52b5a057b73b9974eaa7403e04907f0" #Esto se obtenien en mytelegramorg
-token = "5887442268:AAG-lcurssR91PbCoh-8-oY7coveUdIz8po" #Crea token del bot en botfhater
+api_id = 22398313
+api_hash = "d217c3452766dec8255f568fc6e8934e" #Esto se obtenien en mytelegramorg
+token = "7528897384:AAFNG1NdhDabhBxkZv7xZ9qB6XP7QfNYJcQ" #Crea token del bot en botfhater
